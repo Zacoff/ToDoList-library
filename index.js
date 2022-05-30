c1017c752709c0512be00f7fd26dd5a6d3717a53
Zacoff <daniel.dfa81@gmail.com>
2022-05-30 15:51:41 -0300

    create table

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 import Apperance from "./functions/Apperance.js";
-import Table from "./functions/Table.js";
+import Create from "./functions/Create.js";
 import Validates from "./Validates/Validate.js";
 const header = document.getElementById('header');
 const table = document.getElementById('table')
 
 const titles = ['Name', 'Weigth', 'Birthday#'];
 
-const create = Table.createForm(header,titles);
+const createForm = Create.createForm(header,titles);
+const createTable = Create.createTable(table,titles)
 
-for(let i = 0; i < create[1].length; i++)
-if((create[1][i].id).includes('#')) Validates.dateController(create[1][i])
+for(let i = 0; i < createForm[1].length; i++)
+if((createForm[1][i].id).includes('#')) Validates.dateController(createForm[1][i])
 
-create[0].addEventListener('click', (event) => {
+createForm[0].addEventListener('click', (event) => {
     event.preventDefault();
-    create[1].map(a => {
+    let tr = Create.createElement(createTable,'tr')
+    createForm[1].map(a => {
         const value = a.value;
-        if(a.getAttribute('id').includes('#')) console.log(a.getAttribute('id'));
+        if(a.getAttribute('id').includes('#')) Validates.dateFormat(value);
+        Create.createElement(tr,'th',a.getAttribute('id'),a.value)
     })
 });
